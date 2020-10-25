208ebb0a4640a046de30fdccaace11961aa3b39c
William West <wwest466@gmail.com>
2020-10-25 18:07:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,4 +40,10 @@ attr_reader :id
   end
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+end 
+
 end
