3c602449e71dc6ceafbf1e82099bb2a3c09a6dd6
William West <wwest466@gmail.com>
2020-10-25 18:25:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade
-  attr_accessor :# IDEA:
+  attr_reader :id
     def initialize(id = nil, name, grade)
       @name = name
       @grade = grade
