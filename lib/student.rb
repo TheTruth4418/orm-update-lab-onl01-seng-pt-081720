81e059bd9d25fb748968689f19fc0feb11b34ed5
William West <wwest466@gmail.com>
2020-10-25 18:16:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,10 +48,11 @@ attr_reader :id
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+  self.new(id, name, grade)
+  self.id = row[0]
+  self.name = row[1]
+  self.grade = row[2]
+  
   end
 
   def self.find_by_name(name)
